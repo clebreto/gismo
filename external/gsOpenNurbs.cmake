d65bab221f02792f1ad7610013e89e74456b7adc
Julien Wintz <julien.wintz@inria.fr>
2025-03-05 16:38:18 +0100

    Honor gnu lib dir conventions.

--- external/gsOpenNurbs.cmake
+++ external/gsOpenNurbs.cmake
@@ -21,6 +21,7 @@ if(NOT opennurbs_FOUND)
     message(STATUS "Opennurbs not found, fetching ...")
 
     include(FetchContent)
+    include(GNUInstallDirs)
 
     FetchContent_Declare(
         opennurbs
@@ -52,7 +53,7 @@ if(NOT opennurbs_FOUND)
         endif()
     endif()
 
-    set(OPENNURBS_LIBRARY_PATH "${opennurbs_SOURCE_DIR}/lib/libopennurbs.a")
+    set(OPENNURBS_LIBRARY_PATH "${opennurbs_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libopennurbs.a")
     set(opennurbs_INCLUDE_DIRS ${opennurbs_SOURCE_DIR})
 endif()
 
